ca690edfec78796d9ca2202a465832b76a754721
Nayan <Nayan.Sonipro@gamil.com>
2023-01-13 00:32:03 +0530

    Added

---
+++ AddressBook-main/js/AddressBook.js
@@ -0,0 +1,63 @@
+class AddressBookContact{
+
+    constructor(name,phone,address,city,state,zip){
+        this.name;
+        this.phone;
+        this.address;
+        this.city;
+        this.state;
+        this.zip;
+    }
+    get name() {
+        return this._name;
+    }
+  
+    set name(name) {
+        this._name = name;
+    }
+  
+    get phone() {
+        return this._phone;
+    }
+  
+    set phone(phone) {
+        this._phone = phone;
+    }
+  
+    get address() {
+        return this._address;
+    }
+  
+    set address(address) {
+        this._address = address;
+    }
+  
+    get city() {
+        return this._city;
+    }
+  
+    set city(city) {
+        this._city = city;
+    }
+  
+    get state() {
+        return this._state;
+    }
+  
+    set state(state) {
+        this._state = state;
+    }
+  
+    get zip() {
+        return this._zip;
+    }
+  
+    set zip(zip) {
+        this._zip = zip;
+    }
+  
+    toString() {
+        return 'Name = ' + this.name + ", Phone = " + this.phone + ", Address = " + this.address
+            + ", City = " + this.city + ", State = " + this.state + ", Zip Code = " + this.zip;
+    }
+  }
---
+++ AddressBook-main/js/addressbookform.js
@@ -0,0 +1,80 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+  const name = document.querySelector('#name');
+  const nameError = document.querySelector('.name-error');
+  name.addEventListener('input', function () {
+      let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+      if (nameRegex.test(name.value)) nameError.textContent = "";
+      else nameError.textContent = "Name is Incorrect";
+  })
+
+  const phone = document.querySelector('#phone');
+  const phoneError = document.querySelector('.phone-error');
+  phone.addEventListener('input', function () {
+      let phoneRegex = RegExp('^[+]{0,1}[0-9]{2}\\s{0,1}[0-9]{10}$');
+      if (phoneRegex.test(phone.value)) phoneError.textContent = "";
+      else phoneError.textContent = "Phone Number is Incorrect";
+  })
+
+  const address = document.querySelector('#address');
+  const addressError = document.querySelector('.address-error');
+  address.addEventListener('input', function () {
+      let addressRegex = RegExp('^[a-zA-Z0-9#,&\\s]{4,}$');
+      if (addressRegex.test(address.value)) addressError.textContent = "";
+      else addressError.textContent = "Address is Incorrect";
+  })
+});
+
+//on save
+const save = () => {
+  let contactList = createAddressbook();
+  createAndUpdateStorage(contactList);
+  alert("Details Saved");
+};
+
+const createAddressbook = () => {
+  let contactList = new AddressBookContact();
+
+  contactList.name = getInputValueById('#name');
+  contactList.phone = getInputValueById('#phone');
+  contactList.address = getInputValueById('#address');
+  contactList.city = getInputValueById('#city');
+  contactList.state = getInputValueById('#state');
+  contactList.zip = getInputValueById('#zip');
+  alert("Save");
+  return contactList;
+}
+
+const getInputValueById = (id) => {
+  let value = document.querySelector(id).value;
+  return value;
+}
+
+function createAndUpdateStorage(contactList) {
+  let addressBookList = JSON.parse(localStorage.getItem("AddressBookList"));
+  if (addressBookList != undefined) {
+      addressBookList.push(contactList);
+  } else {
+      addressBookList = [contactList];
+  }
+  alert(addressBookList.toString());
+  localStorage.setItem("AddressBookList", JSON.stringify(addressBookList));
+}
+
+const resetForm = () => {
+  setValue('#name', '');
+  setValue('#phone', '');
+  setValue('#address', '');
+  setValue('#city', '');
+  setValue('#state', '');
+  setValue('#zip', '');
+};
+
+const setTextValue = (id, value) => {
+  const element = document.querySelector(id);
+  element.textContent = value;
+}
+
+const setValue = (id, value) => {
+  const element = document.querySelector(id);
+  element.value = value;
+}
